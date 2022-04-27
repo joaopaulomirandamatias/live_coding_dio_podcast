b11c2e06fc05a2d1ff45d54a97b693131eb9f73e
João Paulo <joaopaulomirandamatias@users.noreply.github.com>
2022-04-27 19:24:53 -0300

    Custom Color

---
+++ lib/app/util/custom_color.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+  static const purple700 = Color(0xFF292537);
+  static const pink700 = Color(0xffE4105D);
+  static const blue750 = Color(0xff249FEF);
+  static const purple750 = Color(0xff5F53D5);
+  static const gray700 = Color(0xff1E192C);
+  static const gray200 = Color(0xffA0A4AE);
+  static const blue200 = Color(0xffECEFF7);
+}
